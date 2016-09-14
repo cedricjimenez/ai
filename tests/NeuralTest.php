fc966a9105bf57b72878790f6989933697ec8cb9
chicha <cjimenez@voyageprive.com>
2016-09-14 03:19:08 +0200

    Unit tests - WIP

---
+++ tests/NeuralTest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * User: cjimenez
+ * Date: 12/09/16 13:59
+ */
+
+namespace Tests;
+
+use ai\Neural\Neural;
+
+
+class NeuralTest extends \UnitTestCase {
+
+    public function testSetType()
+    {
+
+
+        $neural = new Neural();
+        $neural->setType('input');
+
+        $this->assertEquals('input', $neural->getType());
+    }
+
+}
